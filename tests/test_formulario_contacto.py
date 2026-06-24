b4649dd3b1f15d875e2efab8833d18394be61dac
Carmen Pérez <cperezrobles@gmail.com>
2026-06-24 13:01:16 +0200

    POM

--- tests/test_formulario_contacto.py
+++ tests/test_formulario_contacto.py
@@ -1,64 +1,72 @@
 from playwright.sync_api import Page, expect
+from pages.contacto_page import ContactoPage
+
 
 #Enviar formulario con campos obligatorios validos 
+
 def test_formulario_contactos_obligatorios_validos(page: Page):
+
+    contacto_page = ContactoPage(page)
     
     print("Given el usuario abre la página de contacto")
-    page.goto("https://web-qa.dev.adalab.es/contact")
+    contacto_page.visitar_contacto()
 
     print("When rellena el campo nombre con nombre válido")
-    page.get_by_label("Nombre").fill("Reyes Cuesta")
+    contacto_page.rellenar_nombre("Reyes Cuesta")
 
     print("And rellena el campo email con email válido")
-    page.get_by_label("Email").fill("test@gmail.com")
+    contacto_page.rellenar_email("test@gmail.com")
 
     print ("And rellena campo de mensaje")
-    page.get_by_label("Mensaje").fill("Mensaje de prueba")
+    contacto_page.rellenar_mensaje("Mensaje de prueba")
 
     print("And envía el formulario")
-    page.get_by_role("button", name="Enviar Mensaje").click()
+    contacto_page.enviar_mensaje()
 
     print("The debe ver un mensaje de exito")
-    locator = page.get_by_text("¡Mensaje enviado con éxito!")
-    expect(locator).to_be_visible()
+    contacto_page.verificar_mensaje_exito()
 
-    #Test Formulario de Contacto Email invalido
-
+#Enviar formulario email invalido    
 def test_enviar_formulario_email_invalido(page: Page):
+
+    contacto_page = ContactoPage(page)
+
     print("Given el usuario abre la página de contacto")
-    page.goto("https://web-qa.dev.adalab.es/contact")
+    contacto_page.visitar_contacto()
 
     print("When rellena el campo nombre")
-    page.get_by_role("textbox", name="Nombre *").fill("Lluvia aguilar")
+    contacto_page.rellenar_nombre("Lluvia aguilar")
     
     print("And rellena el campo email inválido")
-    page.get_by_role("textbox", name="Email *").fill("test")
+    contacto_page.rellenar_email("test")
 
     print("And rellena el campo mensaje")
-    page.get_by_role("textbox", name="Mensaje *").fill("Mensaje de Prueba")
+    contacto_page.rellenar_mensaje("Mensaje de prueba")
 
     print("And envia el mesaje")
-    page.get_by_role("button", name="Enviar Mensaje").click()
+    contacto_page.enviar_mensaje()
 
     print("Then debe ver un mensaje de error")
-    expect(page.get_by_text("El formato del email no es válido")).to_be_visible()  
+    contacto_page.verificar_mensaje_error_email
 
-    #Test Formulario de Contacto Email vacío
+#Test Formulario de Contacto Email vacío
 def test_enviar_formulario_email_vacio(page: Page):
+
+    contacto_page = ContactoPage(page)
+    
     print("Given el usuario abre la página de contacto")
-    page.goto("https://web-qa.dev.adalab.es/contact")
+    contacto_page.visitar_contacto()
 
     print("When rellena el campo nombre")
-    page.get_by_role("textbox", name="Nombre *").fill("Lluvia aguilar")
+    contacto_page.rellenar_nombre("Lluvia aguilar")
     
     print("And deja el campo email vacío")
-    page.get_by_role("textbox", name="Email *").fill("")
-
+   
     print("And rellena el campo mensaje")
-    page.get_by_role("textbox", name="Mensaje *").fill("Mensaje de Prueba")
+    contacto_page.rellenar_mensaje("Mensaje de prueba")
 
     print("And envia el mesaje")
-    page.get_by_role("button", name="Enviar Mensaje").click()
+    contacto_page.enviar_mensaje()
 
     print("Then debe ver un mensaje de error")
-    expect(page.get_by_text("El email es obligatorio")).to_be_visible()
+    contacto_page.verificar_mensaje_error_email_obligatorio()
