c4d89f9e393638ba24fe501d1081cdf071e1c3da
Carmen Pérez <cperezrobles@gmail.com>
2026-06-24 11:30:30 +0200

    POM

--- tests/test_productos.py
+++ tests/test_productos.py
@@ -1,22 +1,25 @@
 from playwright.sync_api import Page, expect
-
+from pages.productos_page import ProductosPage
 
 def test_ver_productos_y_detalles(page: Page):
+
+    productos_page = ProductosPage(page)
+
     print("Given la usuaria abre la página de productos")
-    page.goto("https://web-qa.dev.adalab.es/products")
+    productos_page.visitar_productos()
 
     print("Then la usuaria debe ver el título 'Catálogo de Productos'")
     # Comprobamos que la página contiene el título "Catálogo de Productos"
-    expect(page.get_by_role("heading", name="Catálogo de Productos")).to_be_visible()
+    productos_page.verificar_titulo()
 
     print("And la usuaria debe ver la categoría del producto 'Plantas'")
     # Comprobamos que la usuaria ve la categoría del producto "Plantas"
-    expect(page.get_by_text("Plantas").nth(2)).to_be_visible()
+    productos_page.verificar_categoria("Plantas")
 
     print("And la usuaria debe ver el nombre del producto 'Ficus Lyrata'")
-    # Comprobamos que la usuaria ve el nombre del producto "Fycus Lyrata"
-    expect(page.get_by_role("heading", name="Ficus Lyrata")).to_be_visible()
+    # Comprobamos que la usuaria ve el nombre del producto "Ficus Lyrata"
+    productos_page.verificar_nombre_producto("Ficus Lyrata")
 
     print("And la usuaria debe ver el precio del producto '35.00 €'")
     # Comprobamos que la usuaria ve el precio del producto "35.00 €"
-    expect(page.get_by_text("35.00 €")).to_be_visible()
+    productos_page.verificar_precio_producto("35.00 €")
